473fc7d7e76fffa4e9f09ac2c7551c04a4345e9f
nedviga_d_yu <DenAftogen@mail.ru>
2026-04-21 12:59:27 +0300

    exe

--- main-windows.cpp
+++ main-windows.cpp
@@ -1,70 +1,63 @@
 #include <iostream>
+#include <windows.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_HOTKEY:
+            if (wParam == 1001) {
+                std::cout << "Hotkey Ctrl+Shift+H pressed!\n";
+            }
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+    return DefWindowProcW(hwnd, uMsg, wParam, lParam);
 }
 
-// // global_hotkey_win.cpp
-// #include <iostream>
-// #include <windows.h>
+int main() {
+    const char CLASS_NAME[] = "HotkeyConsoleWindow";
 
-// LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-//     switch (uMsg) {
-//         case WM_HOTKEY:
-//             if (wParam == 1001) {
-//                 std::cout << "Hotkey Ctrl+Shift+H pressed!\n";
-//             }
-//             break;
-//         case WM_DESTROY:
-//             PostQuitMessage(0);
-//             return 0;
-//     }
-//     return DefWindowProcW(hwnd, uMsg, wParam, lParam);
-// }
+    WNDCLASSA wc = {};
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = GetModuleHandleA(nullptr);
+    wc.lpszClassName = CLASS_NAME;
 
-// int main() {
-//     const char CLASS_NAME[] = "HotkeyConsoleWindow";
+    if (!RegisterClassA(&wc)) {
+        std::cerr << "RegisterClass failed\n";
+        return 1;
+    }
 
-//     WNDCLASSA wc = {};
-//     wc.lpfnWndProc   = WindowProc;
-//     wc.hInstance     = GetModuleHandleA(nullptr);
-//     wc.lpszClassName = CLASS_NAME;
+    HWND hwnd = CreateWindowExA(
+        0,
+        CLASS_NAME,
+        "Hotkey Handler",
+        0, 0, 0, 0, 0,
+        HWND_MESSAGE,
+        nullptr,
+        wc.hInstance,
+        nullptr
+    );
 
-//     if (!RegisterClassA(&wc)) {
-//         std::cerr << "RegisterClass failed\n";
-//         return 1;
-//     }
+    if (!hwnd) {
+        std::cerr << "CreateWindow failed\n";
+        return 1;
+    }
 
-//     HWND hwnd = CreateWindowExA(
-//         0,
-//         CLASS_NAME,
-//         "Hotkey Handler",
-//         0, 0, 0, 0, 0,
-//         HWND_MESSAGE,
-//         nullptr,
-//         wc.hInstance,
-//         nullptr
-//     );
+    // Регистрируем Ctrl+Shift+H (id = 1001)
+    if (!RegisterHotKey(hwnd, 1001, MOD_CONTROL | MOD_SHIFT, 'H')) {
+        std::cerr << "RegisterHotKey failed\n";
+        return 1;
+    }
 
-//     if (!hwnd) {
-//         std::cerr << "CreateWindow failed\n";
-//         return 1;
-//     }
+    std::cout << "Global hotkey Ctrl+Shift+H registered. Press Ctrl+C in the console to exit.\n";
 
-//     // Регистрируем Ctrl+Shift+H (id = 1001)
-//     if (!RegisterHotKey(hwnd, 1001, MOD_CONTROL | MOD_SHIFT, 'H')) {
-//         std::cerr << "RegisterHotKey failed\n";
-//         return 1;
-//     }
+    MSG msg = {};
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
-//     std::cout << "Global hotkey Ctrl+Shift+H registered. Press Ctrl+C to exit.\n";
-
-//     MSG msg = {};
-//     while (GetMessage(&msg, nullptr, 0, 0)) {
-//         TranslateMessage(&msg);
-//         DispatchMessage(&msg);
-//     }
-
-//     UnregisterHotKey(hwnd, 1001);
-//     return 0;
-// }
+    UnregisterHotKey(hwnd, 1001);
+    return 0;
+}
