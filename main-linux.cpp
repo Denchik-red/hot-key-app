3e5e5ea873336550c5d535a7bbb114149d9cf548
nedviga_d_yu <DenAftogen@mail.ru>
2026-04-21 12:02:35 +0300

    init

---
+++ main-linux.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+    Display *display = XOpenDisplay(NULL);
+    if (!display) {
+        std::cerr << "Не удалось открыть display" << std::endl;
+        return 1;
+    }
+
+    Window root = XDefaultRootWindow(display);
+    KeyCode keycode = XKeysymToKeycode(display, XStringToKeysym("Y"));
+    XGrabKey(display, keycode, ControlMask, root, False, GrabModeAsync, GrabModeAsync);
+    XFlush(display);
+
+    std::cout << "Глобальный hotkey Ctrl+Y зарегистрирован. Нажмите для теста." << std::endl;
+
+    XEvent event;
+    while (true)
+    {
+        XNextEvent(display, &event);
+        if (event.type == KeyPress)
+        {
+            std::cout << "Hotkey Ctrl+Y нажат!" << std::endl;
+            // Ваша логика здесь
+            break; // Для выхода; удалите для постоянной работы
+        }
+    }
+
+    XUngrabKey(display, keycode, ControlMask, root);
+    XCloseDisplay(display);
+    return 0;
+}
