86065292e58924a9a33aeeb28c02f0de6f488db8
nedviga_d_yu <DenAftogen@mail.ru>
2026-04-24 09:42:51 +0300

    setup hotkeys

--- setun-gnome-hotkey.sh
+++ setun-gnome-hotkey.sh
@@ -3,18 +3,19 @@ HOTKEY_NAME="Ctrl+Y Hotkey"
 HOTKEY_COMMAND="dbus-send --session --dest=ru.example.Hotkey /Hotkey ru.example.Hotkey.HandleHotkey"
 HOTKEY_BINDING="Control+y"
 
-# Получить текущий список
-CUSTOM_KEYS=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings | sed "s/[[\]//g" | sed "s/[\]]//g" | sed "s/'/ /g")
+# 1. Подсчитать количество существующих
+COUNT=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings | grep -o 'custom[0-9]\+' | wc -l)
 
-# Добавить новый ключ
-NEW_PATH="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom${#CUSTOM_KEYS}/"
-echo $CUSTOM_KEYS $NEW_PATH | tr ' ' '\n' | grep -v '^$' | tr '\n' ',' | sed 's/,$//'
+# 2. Новый слот
+NEW_SLOT=$COUNT
+NEW_PATH="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom${NEW_SLOT}/"
 
-gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "[$NEW_PATH]"
+# 3. Добавить к существующим
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['$NEW_PATH']"
 
-# Настроить
+# 4. Настроить
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$NEW_PATH name "$HOTKEY_NAME"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$NEW_PATH command "$HOTKEY_COMMAND"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$NEW_PATH binding "$HOTKEY_BINDING"
 
-echo "✅ Hotkey $HOTKEY_BINDING установлен!"
+echo "✅ Hotkey $HOTKEY_BINDING = custom$NEW_SLOT"
